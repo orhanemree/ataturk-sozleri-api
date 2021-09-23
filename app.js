a1dd8f294e374fdd3f183c4c7d6eb5dc00fe58e6
Orhan Emre <orhanemre.dev@gmail.com>
2021-09-23 21:40:16 +0300

    183 quotes added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const app = require('express')();
+const quotes = require('./quotes.json');
 
 app.get("/", (req, res) => {
-    res.end("Hello, World.");
+    res.json(quotes[Math.floor(Math.random() * quotes.length)]);
+    res.writeHead(200, { 'Content-Type': 'application/json'});
 });
 
 app.listen(8080);
